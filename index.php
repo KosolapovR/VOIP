fc93433044782aab672e1d6f33fc2e58ec87ed33
Kosolapov Roman <Kosolapov-r@bk.ru>
2019-08-04 18:59:26 +0300

    footer before mobile version

--- index.php
+++ index.php
@@ -21,9 +21,9 @@
       <div class="header-conteiner-items logo-conteiner">
           <span>Ваш регион: <a href="" id="region">Москва</a></span>
           <div class="logo">
-              <div class="ho">ho</div>
-              <div class="my"></div>
-              <div class="ru"></div>
+              <div class="logo_item ho">ho</div>
+              <div class="logo_item my secondary">my</div>
+              <div class="logo_item ru">ru</div>
           </div>
           <p>Оборудование и решение для voip</p>
       </div>
@@ -619,7 +619,11 @@
        <div class="top_footer">
            <div class="top_footer_whraper justify">
                <div class="col col1">
-                   <img id="logo-container" src="images/logo.png" alt="logo">
+                   <div class="logo">
+              <div class="logo_item ho">ho</div>
+              <div class="logo_item my secondary">my</div>
+              <div class="logo_item ru">ru</div>
+          </div>
                    <p>                  	&#169 ХомиРу, 2006-2019 <br>
                    Оборудование и решения для VoIP
                    </p>
@@ -650,12 +654,59 @@
          <img src="icons/conversation.svg" alt="callback">
           <span class="callback-text"><a href="">Обратный звонок</a></span>
       </div>
-                   <div class="header-conteiner-items contact-info">
+                   <div class="contact-info">
           <p class="number">+7 495 2-55-44-66</p>
           <p class="hours">(пн-пт с 9 до 18 ч)</p>
           <p class="number2"><i class="fab fa-whatsapp"></i><i class="fab fa-viber"></i>+7 903 111-11-11</p>
           <p class="skype"><i class="fab fa-skype"></i> skype-on.06</p>
           <p class="mail"><a href="">info@nomy.ru</a></p>
+          <div class="social">
+              <div class="social_item vk">
+                 <div class="social_item_whitebox">
+                     <svg version="1.1" id="Capa_1"  xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	              viewBox="0 0 96 96" style="enable-background:new 0 0 96 96;" xml:space="preserve">
+                  <g>
+	<path style="fill:#325278;" d="M53.174,52.908c-1.539-0.896-3.678-0.896-5.416-0.896h-5.707v11.76h5.238
+		c1.939,0,4.412,0.139,6.084-1.035c1.535-1.033,2.404-3.061,2.404-4.992C55.777,55.951,54.709,53.805,53.174,52.908z M51.021,42.865
+		c1.27-0.967,1.936-2.691,1.936-4.279c0-1.725-0.801-3.381-2.27-4.277c-1.537-0.896-4.346-0.689-6.15-0.689h-2.486v10.35h3.289
+		C47.211,43.969,49.418,44.107,51.021,42.865z M79.125,0H16.873C7.555,0,0,7.555,0,16.875v62.25C0,88.443,7.555,96,16.873,96h62.252
+		C88.443,96,96,88.443,96,79.125v-62.25C96,7.555,88.443,0,79.125,0z M50.604,72.535H30.992V25.201H52.42
+		c6.207,0,11.934,3.951,11.934,10.92c0,5.381-3.004,9.066-6.836,10.211v0.139c5.682,1.174,9.74,4.26,9.74,11.705
+		C67.258,65.066,62.566,72.535,50.604,72.535z"/>
+</g>
+                 </svg>
+                 </div>
+                   
+              </div>
+              <div class="social_item facebook">
+                 <div class="social_item_whitebox">
+                  <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	              viewBox="0 0 486.392 486.392" style="enable-background:new 0 0 486.392 486.392;" xml:space="preserve">
+                  <g>
+			<path style="fill:#2E428B;" d="M395.193,0H91.198C40.826,0,0,40.826,0,91.198v303.995c0,50.372,40.826,91.198,91.198,91.198
+				h303.995c50.372,0,91.198-40.827,91.198-91.198V91.198C486.392,40.826,445.565,0,395.193,0z M306.062,243.165l-39.854,0.03
+				l-0.03,145.917h-54.689V243.196H175.01v-50.281l36.479-0.03l-0.061-29.609c0-41.039,11.126-65.997,59.431-65.997h40.249v50.311
+				h-25.171c-18.817,0-19.729,7.022-19.729,20.124l-0.061,25.171h45.234L306.062,243.165z"/>
+		</g>
+                  </svg>
+                  </div>
+              </div>
+              <div class="social_item twitter">
+                  <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	              viewBox="0 0 455.731 455.731" style="enable-background:new 0 0 455.731 455.731;" xml:space="preserve">
+                  <g>
+	<rect x="0" y="0" style="fill:#50ABF1;" width="455.731" height="455.731"/>
+	<path style="fill:#FFFFFF;" d="M60.377,337.822c30.33,19.236,66.308,30.368,104.875,30.368c108.349,0,196.18-87.841,196.18-196.18
+		c0-2.705-0.057-5.39-0.161-8.067c3.919-3.084,28.157-22.511,34.098-35c0,0-19.683,8.18-38.947,10.107
+		c-0.038,0-0.085,0.009-0.123,0.009c0,0,0.038-0.019,0.104-0.066c1.775-1.186,26.591-18.079,29.951-38.207
+		c0,0-13.922,7.431-33.415,13.932c-3.227,1.072-6.605,2.126-10.088,3.103c-12.565-13.41-30.425-21.78-50.25-21.78
+		c-38.027,0-68.841,30.805-68.841,68.803c0,5.362,0.617,10.581,1.784,15.592c-5.314-0.218-86.237-4.755-141.289-71.423
+		c0,0-32.902,44.917,19.607,91.105c0,0-15.962-0.636-29.733-8.864c0,0-5.058,54.416,54.407,68.329c0,0-11.701,4.432-30.368,1.272
+		c0,0,10.439,43.968,63.271,48.077c0,0-41.777,37.74-101.081,28.885L60.377,337.822z"/>
+</g>
+                  </svg>
+              </div>
+          </div>
           
       </div>
               </div>
